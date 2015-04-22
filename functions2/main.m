cae69c77c0b87ca9b3381da9600b1e6d9069c741
Your Name <everettchurchey@crbc.edu>
2015-04-22 09:15:06 -0400

    Initial Commit

---
+++ functions2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  functions2
+//
+//  Created by Ej Churchey on 4/22/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
